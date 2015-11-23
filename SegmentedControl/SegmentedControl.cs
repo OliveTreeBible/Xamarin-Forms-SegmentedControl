04e5ab64bed42d8410c2cd2be2fb2c4e513dea9f
Tom Hamming <tomhamming@olivetree.com>
2015-11-23 12:49:19 -0800

    File formatting, fix compile error

--- SegmentedControl/SegmentedControl.cs
+++ SegmentedControl/SegmentedControl.cs
@@ -4,42 +4,42 @@ using System.Collections.Generic;
 
 namespace SegmentedControl
 {
-	public class SegmentedControl : View, IViewContainer<SegmentedControlOption>
-	{
-		public IList<SegmentedControlOption> Children { get; set; }
+    public class SegmentedControl : View, IViewContainer<SegmentedControlOption>
+    {
+        public IList<SegmentedControlOption> Children { get; set; }
 
-		public SegmentedControl ()
-		{
-			Children = new List<SegmentedControlOption> ();
-		}
+        public SegmentedControl()
+        {
+            Children = new List<SegmentedControlOption>();
+        }
 
-		public event ValueChangedEventHandler ValueChanged;
+        public event ValueChangedEventHandler ValueChanged;
 
-		public delegate void ValueChangedEventHandler (object sender, EventArgs e);
+        public delegate void ValueChangedEventHandler(object sender, EventArgs e);
 
-		private string selectedValue;
+        private string selectedValue;
 
-		public string SelectedValue {
-			get{ return selectedValue; }
-			set {
-				selectedValue = value;
-				if (ValueChanged != null)
-					ValueChanged (this, EventArgs.Empty);
-			}
-		}
-	}
+        public string SelectedValue {
+            get{ return selectedValue; }
+            set {
+                selectedValue = value;
+                if (ValueChanged != null)
+                    ValueChanged(this, EventArgs.Empty);
+            }
+        }
+    }
 
-	public class SegmentedControlOption:View
-	{
-		public static readonly BindableProperty TextProperty = BindableProperty.Create<SegmentedControlOption, string> (p => p.Text, "");
+    public class SegmentedControlOption:View
+    {
+        public static readonly BindableProperty TextProperty = BindableProperty.Create<SegmentedControlOption, string>(p => p.Text, "");
 
-		public string Text {
-			get{ return (string)GetValue (TextProperty); }
-			set{ SetValue (TextProperty, value); }
-		}
+        public string Text {
+            get{ return (string)GetValue(TextProperty); }
+            set{ SetValue(TextProperty, value); }
+        }
 
-		public SegmentedControlOption ()
-		{
-		}
-	}
+        public SegmentedControlOption()
+        {
+        }
+    }
 }
--- iOS/SegmentedControlRenderer.cs
+++ iOS/SegmentedControlRenderer.cs
@@ -1,32 +1,33 @@
 ﻿using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.iOS;
-using MonoTouch.UIKit;
+using UIKit;
 
 [assembly:ExportRenderer(typeof(SegmentedControl.SegmentedControl), typeof(SegmentedControl.iOS.SegmentedControlRenderer))]
 namespace SegmentedControl.iOS
 {
-	public class SegmentedControlRenderer : ViewRenderer<SegmentedControl, UISegmentedControl>
-	{
-		public SegmentedControlRenderer ()
-		{
-		}
+    public class SegmentedControlRenderer : ViewRenderer<SegmentedControl, UISegmentedControl>
+    {
+        public SegmentedControlRenderer()
+        {
+        }
 
-		protected override void OnElementChanged (ElementChangedEventArgs<SegmentedControl> e)
-		{
-			base.OnElementChanged (e);
+        protected override void OnElementChanged(ElementChangedEventArgs<SegmentedControl> e)
+        {
+            base.OnElementChanged(e);
 
-			var segmentedControl = new UISegmentedControl ();
+            var segmentedControl = new UISegmentedControl();
 
-			for (var i = 0; i < e.NewElement.Children.Count; i++) {
-				segmentedControl.InsertSegment (e.NewElement.Children [i].Text, i, false);
-			}
+            for (var i = 0; i < e.NewElement.Children.Count; i++)
+            {
+                segmentedControl.InsertSegment(e.NewElement.Children[i].Text, i, false);
+            }
 
-			segmentedControl.ValueChanged += (sender, eventArgs) => {
-				e.NewElement.SelectedValue = segmentedControl.TitleAt(segmentedControl.SelectedSegment);
+            segmentedControl.ValueChanged += (sender, eventArgs) => {
+                e.NewElement.SelectedValue = segmentedControl.TitleAt(segmentedControl.SelectedSegment);
 			};
 
-			SetNativeControl (segmentedControl);
-		}
-	}
+            SetNativeControl(segmentedControl);
+        }
+    }
 }
